fd4b2e13ac8c8611b164ded80e96367c7df6732c
lakshmiprasad1987 <41810610+lakshmiprasad1987@users.noreply.github.com>
2019-03-31 10:43:31 +0800

    Update StudentRestController.java

--- src/main/java/com/bp/SpringMicro/StudentRestController.java
+++ src/main/java/com/bp/SpringMicro/StudentRestController.java
@@ -3,17 +3,22 @@ package com.bp.SpringMicro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class StudentRestController {
 	
 	 @Autowired
 	 private  ApplicationConfig _applicationConfig;
+	
+	//This is not required , just for tetsing other ways
+	@Value("${application.env}")
+        private String environment;
 	 
 	 
 	@GetMapping("/students/getEnv")
 	public String retrieveEnvironmentForStudent() {
-		return _applicationConfig.getEnv()+"-"+_applicationConfig.getMessage();
+		return _applicationConfig.getEnv()+"-"+_applicationConfig.getMessage()+"---"+environment;
 	}
 
 }
